19a12c4abc79541110aaf3ed6041f0f898fc9b8a
Danie206 <128877073+Danie206@users.noreply.github.com>
2024-12-19 23:17:20 -0500

    Add chapter_2 project

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const PORT = 8383
+console.log("This is an extra line")
+ 
+app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
