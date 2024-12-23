b91c4751a082c754e7d298cdb9925d8c05ca3cce
Danie206 <128877073+Danie206@users.noreply.github.com>
2024-12-23 07:44:25 -0500

    learned basics on backend

--- chapter-2/server.js
+++ chapter-2/server.js
@@ -2,20 +2,43 @@
 const express = require('express')
 const app = express()
 const PORT = 8383
-// HTTP VERBS && ROUTES (or paths)
-//The method informs the nature of reuqest and the route is a further subdirectory 
-// (basically we direct the ruest to the body of code to respond appropriately, and
-//  these locations or routes are called enpoints)
 
- 
-app.get('/', (req, res) => { // this is endpoint number 1-// 
-res.send('<h1>homepage</h1>')
+let data = ['james']
+// Middleware
+app.use(express.json())
+// HTTP VERBS && ROUTES (or paths)
+// The method informs the nature of request and the route is a further subdirectory 
+// (basically we direct the request to the body of code to respond appropriately, and
+// these locations or routes are called endpoints)
+
+app.get('/dashboard', (req, res) => {
+  res.send('<body><h1>dashboard</h1><a href="/">home</a></body>')
 })
-app.get('/dashboard', (req, res) => { // this is endpoint number 1-// 
-  res.send('<h1>dashboard</h1>')
-  })
-  app.get('/api/data', (req, res) => {
-    console.log('This one was for data')
-    res.send(data)
-  })
+
+app.get('/', (req, res) => {
+  res.send(`
+    <body style="background:pink;
+    color: blue;"><h1>DATA:</h1><p>${JSON.stringify(data)}</p><a href="/dashboard">Dashboard</a></body>`)
+})
+
+// Type 2 - API endpoints (non-visual)
+// CRUD create-POST read-GET update-PUT delete-DELETE
+
+app.get('/api/data', (req, res) => {
+  console.log('This one was for data')
+  res.send(data)
+})
+
+app.post('/api/data', (req, res) => {
+  const newEntry = req.body
+  console.log(newEntry)
+  data.push(newEntry.name)
+  res.sendStatus(201)
+})
+app.delete('/api/data', (req, res) => {
+  data.pop()
+  console.log('Deleted')
+  res.sendStatus(203)
+})
+
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
